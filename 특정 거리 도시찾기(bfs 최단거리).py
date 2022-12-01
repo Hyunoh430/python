d4fc4de340da965ad9d449857a1fdb4ba03fff7f
Hyunoh430 <116778172+Hyunoh430@users.noreply.github.com>
2022-12-01 11:10:24 +0900

    bfs기본
    input을 이런 식으로 받는다 배웠다.
    distance 0이 아닌 -1로 초기화해야되는 이유 생각!

---
+++ 특정 거리 도시찾기(bfs 최단거리).py
@@ -0,0 +1,28 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n + 1)           #여기서 -1로 초기화 하지 않고 0으로 초기화 시키고 한다면 출발점으로 향하는 것이 있다면 다시 돌아옴!
+distance[x] = 0
+
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
